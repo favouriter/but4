53f8e5a24b8ee161441f236be3d41860a2cc8f79
steve <ming.li@firedata.cn>
2023-10-08 10:50:42 +0800

    add check_web

--- fast_host.py
+++ fast_host.py
@@ -1,8 +1,8 @@
-import asyncio, time
+import asyncio, aiohttp, time
 import dns.asyncresolver
 
 # 检测IP、端口打开关闭的速度
-async def check_ip(domain, ip, port=443):
+async def check_ip(domain, ip, session, port=443):
     try:
         start = time.time()
         t = asyncio.open_connection(ip, port)
@@ -10,9 +10,23 @@ async def check_ip(domain, ip, port=443):
         writer.close()
         await writer.wait_closed()
         dey = time.time() - start
-        print(f'{domain:<25}\t\t{ip:<20}\t\t{dey}')
+        print(f'{domain:<25}\t\t{ip:<20}\t\t{dey:.3f}')
         return ip, dey
     except:
+        return ip, 60
+
+# 检查web打开速度
+async def check_web(domain, ip, session, port=443):
+    try:
+        headers=dict(Host=domain,Origin=f'https://{domain}')
+        start = time.time()
+        async with session.get(f'https://{ip}:{port}', headers=headers, ssl=False) as response:
+            dey = time.time() - start
+            print(f'{domain:<25}\t\t{ip:<20}\t\t{dey:.3f}')
+            return ip, dey
+        return ip, 60
+    except Exception as e:
+        print(str(e))
         return ip, 60
 
 # 使用域名服务器解析查询
@@ -47,7 +61,7 @@ async def get_ip_by_localdns(domain):
     return await get_ip_by_dns(domain)
 
 # 根据域名查找最快的IP
-async def get_fast(domain, port = 443):
+async def get_fast(domain, session, port = 443, check=check_web):
     # 汇总不同的查询途径
     ips = await asyncio.gather(
         get_ip_by_alidns(domain),
@@ -58,7 +72,7 @@ async def get_fast(domain, port = 443):
     )
     ips = set([ip for isub in ips for ip in isub])
     print(domain, ips)
-    check_ips = await asyncio.gather(*[check_ip(domain, ip, port=port) for ip in ips])
+    check_ips = await asyncio.gather(*[check_web(domain, ip, session, port=port) for ip in ips])
     check_ips = [ip for ip in check_ips if ip[1] < 60]
     if len(check_ips) == 0: return domain, None
     fast_ip = sorted(check_ips, key=lambda x: x[1])[0][0]
@@ -68,9 +82,10 @@ async def get_fast(domain, port = 443):
 # 批量检测域名
 async def check_domains(addresses):
     # fast_ips = await asyncio.gather(*[get_fast(addr[0], port=addr[1]) for addr in addresses])
+    session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False))
     fast_ips = []
     for addr in addresses:
-        fast_ips.append(await get_fast(addr[0], port=addr[1]))
+        fast_ips.append(await get_fast(addr[0], session, port=addr[1], check=addr[2]))
         await asyncio.sleep(1)
     with open('./out/fast_host.txt', 'w', encoding='utf-8') as f:
         for addr in fast_ips:
@@ -81,24 +96,24 @@ async def check_domains(addresses):
 
 if __name__ == '__main__':
     addresses = [
-        ('api.github.com', 443),
-        ('codeload.github.com', 443),
-        ('collector.github.com', 443),
-        ('pipelines.actions.githubusercontent.com', 443),
-        ('media.githubusercontent.com', 443),
-        ('cloud.githubusercontent.com', 443),
-        ('objects.githubusercontent.com', 443),
-        ('github.com', 443),
-        ('raw.githubusercontent.com', 443),
-        ('github.global.ssl.fastly.net', 443),
-        ('assets-cdn.github.com', 443),
-        ('github.githubassets.com', 443),
-        ('huggingface.co', 443),
-        ('cdnjs.cloudflare.com', 443),
-        ('m.stripe.com', 443),
-        ('fonts.gstatic.com', 443),
-        ('aeiljuispo.cloudimg.io', 443),
-        ('cdn-lfs.huggingface.co', 443),
+        # ('api.github.com', 443, check_web),
+        # ('codeload.github.com', 443, check_web),
+        # ('collector.github.com', 443, check_web),
+        # ('pipelines.actions.githubusercontent.com', 443, check_web),
+        # ('media.githubusercontent.com', 443, check_web),
+        # ('cloud.githubusercontent.com', 443, check_web),
+        # ('objects.githubusercontent.com', 443, check_web),
+        ('github.com', 443, check_web),
+        # ('raw.githubusercontent.com', 443, check_web),
+        # ('assets-cdn.github.com', 443, check_web),
+        # ('github.githubassets.com', 443, check_web),
+        # ('huggingface.co', 443, check_web),
+        # ('cdnjs.cloudflare.com', 443, check_web),
+        # ('m.stripe.com', 443, check_web),
+        # ('fonts.gstatic.com', 443, check_web),
+        # ('aeiljuispo.cloudimg.io', 443, check_web),
+        ('cdn-lfs.huggingface.co', 443, check_web),
+        ('hf.co',22, check_ip),
     ]
 
     loop = asyncio.get_event_loop()
